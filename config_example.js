73c64b1b199a39fd7cf531ff78f701f826afdf7c
Kumar Anirudha <anirudhastark@yahoo.com>
2017-07-23 15:58:54 +0530

    initial commit

--- config_example.js
+++ config_example.js
@@ -1,18 +1,8 @@
 module.exports = {
-  // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'Blockchained India',
-  // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'blockchainedindia.slack.com',
-  // access token of slack
-  // You can generate it in https://api.slack.com/web#auth
-  // You should generate the token in admin user, not owner.
-  // If you generate the token in owner user, missing_scope error will be occurred.
-  //
-  // You can test your token via curl:
-    // curl -X POST 'https://blockchainedindia.slack.com/api/users.admin.invite' --data 'email=anirudhastark@yahoo.com&token=TOKEN&set_active=true' --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'TOKEN',
-  // an optional security measure - if it is set, then that token will be required to get invited.
-  inviteToken: process.env.INVITE_TOKEN || null,
-
-  locale: process.env.LOCALE || "en",
+    port: '3000',
+    community: process.env.COMMUNITY_NAME || 'Blockchained India',
+    slackUrl: process.env.SLACK_URL || 'blockchainedindia.slack.com',
+    slacktoken: process.env.SLACK_TOKEN || 'TOKEN',
+    inviteToken: process.env.INVITE_TOKEN || null,
+    locale: process.env.LOCALE || "en",
 };
